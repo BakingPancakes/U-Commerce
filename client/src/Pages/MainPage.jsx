3455dafa3c0807ba59eafea7d5e8691be95ea10b
ohosman04 <omargawad9000@gmail.com>
2026-04-10 17:18:18 -0400

    Main page first draft

--- client/src/Pages/MainPage.jsx
+++ client/src/Pages/MainPage.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from 'react'
 import Navbar from '../Components/Navbar'
 import './MainPage.css'
 import hampcollpic from "../assets/hamp-camp-pic.jpg"
-import umasspic from "../assets/hamp-camp-pic.jpg"
-import holyokepic from "../assets/hamp-camp-pic.jpg"
-import smithpic from "../assets/hamp-camp-pic.jpg"
-import amcolpic from "../assets/hamp-camp-pic.jpg"
+import umasspic from "../assets/umass-camp-pic.jpg"
+import holyokepic from "../assets/holyoke-camp-pic.jpg"
+import smithpic from "../assets/smith-camp-pic.jpg"
+import amcolpic from "../assets/amherstcoll-camp-pic.jpg"
 
 function MainPage() {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentSlideIndex((prevIndex + 1) % 5)
+      setCurrentSlideIndex(prevIndex => (prevIndex + 1) % 5)
     }, 5000) // Change slide every 5 seconds
 
     return () => clearInterval(timer)
@@ -31,18 +31,17 @@ function MainPage() {
       <Navbar />
       <main>
         {/* Slideshow Section */}
-        <section className="slideshow-container">
-          {slides.map((src, index) => (
-            <div key={index} className="slide-image">
-              <img src={src} alt={`Slide ${index + 1}`} className="slide" />
+        <div className="hero-section">
+          <section className="slideshow-container">
+            <div className="slide-image">
+              <img src={slides[currentSlideIndex]} alt="Slideshow" className="slide" />
             </div>
-          ))}
-        </section>
+          </section>
 
-        {/* Welcome Header */}
-        <header className="welcome-header">
-          <h1>Welcome to U-Commerce</h1>
-        </header>
+          <header className="welcome-header">
+            <h1>Welcome to U-Commerce</h1>
+          </header>
+        </div>
 
         {/* About Section */}
         <section className="about-section">
