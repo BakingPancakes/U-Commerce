3efc624ba126c8b8dbec5f6db26cc5a3fea8bd51
Rudraksh22 <rudrakshshar@umass.edu>
2026-05-03 15:51:26 -0400

    Added frontend for profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import ListingsPage from './Pages/ListingsPage'
 import LoginPage from './Pages/LoginPage'
 import ListingDetailPage from './Pages/ListingDetailPage'
 import ListingFormPage from './Pages/ListingFormPage'
+import ProfilePage from './Pages/ProfilePage'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       {/* You can add other routes here later */}
       <Route path="/listings" element={<ListingsPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
       <Route path="/listings/new" element={<ListingFormPage mode="create" />} />
       <Route path="/listings/:id" element={<ListingDetailPage />} />
       <Route path="/listings/:id/edit" element={<ListingFormPage mode="edit" />} />
